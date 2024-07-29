a5e6016c69ba5c6fb9ef61442dd32de212882d57
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2024-07-29 22:24:34 +0900

    サーバー:自己完結型バイナリに修正

--- make-server-release.sh
+++ make-server-release.sh
@@ -5,20 +5,10 @@ echo "Building Server..."
 rm -rf bin/server
 
 cd release-display-server
-dotnet build -c Release
+dotnet publish -r win-x64 --self-contained
 
 # copy files
 mkdir ../bin/server
-cp bin/Release/net8.0/appsettings.json ../bin/server/appsettings.json
-cp bin/Release/net8.0/Microsoft.OpenApi.dll ../bin/server/Microsoft.OpenApi.dll
-cp bin/Release/net8.0/Microsoft.AspNetCore.OpenApi.dll ../bin/server/Microsoft.AspNetCore.OpenApi.dll
-cp bin/Release/net8.0/package-lock.json ../bin/server/package-lock.json
-cp bin/Release/net8.0/vue_gamepad_server.dll ../bin/server/vue_gamepad_server.dll
-cp bin/Release/net8.0/vue_gamepad_server.exe ../bin/server/vue_gamepad_server.exe
-cp bin/Release/net8.0/vue_gamepad_server.runtimeconfig.json ../bin/server/vue_gamepad_server.runtimeconfig.json
-cp bin/Release/net8.0/Swashbuckle.AspNetCore.Swagger.dll ../bin/server/Swashbuckle.AspNetCore.Swagger.dll
-cp bin/Release/net8.0/Swashbuckle.AspNetCore.SwaggerGen.dll ../bin/server/Swashbuckle.AspNetCore.SwaggerGen.dll
-cp bin/Release/net8.0/Swashbuckle.AspNetCore.SwaggerUI.dll ../bin/server/Swashbuckle.AspNetCore.SwaggerUI.dll
-cp bin/Release/net8.0/vue_gamepad_server.deps.json ../bin/server/vue_gamepad_server.deps.json
+cp bin/Release/net8.0/win-x64/publish/* ../bin/server
 
 echo "Server built complete."
